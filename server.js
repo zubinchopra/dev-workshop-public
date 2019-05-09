48d3638c1fb21ddc98d7639fc2c2d2fbad607dad
zubinchopra <zubin.msvv@gmail.com>
2019-05-09 16:43:28 -0700

    Added server code

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 const Message = require('./models/message');
 const socket = require('socket.io');
+const path = require('path');
 
 const PORT = process.env.PORT || 8080;
 
@@ -50,6 +51,13 @@ app.post('/api/message', (req, res) => {
         res.send(err).status(500);
     });
 });
+
+if(process.env.NODE_ENV === "production") {
+    app.use(express.static("./client/build"));
+    app.get("*", (req, res) => {
+        res.sendFile(path.resolve(__dirname, "./client/build/index.html"));
+    });
+}
 
 // Start the server at the specified PORT
 let server = app.listen(PORT, () => {
